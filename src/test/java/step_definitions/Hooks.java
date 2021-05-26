f0e15b1c246c667cd9f692d03be6d534369d6951
Noorl <ab427902@dal.ca>
2021-05-26 18:28:30 -0400

    Cucumber for Jenkins

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -3,28 +3,64 @@ package step_definitions;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class Hooks {
+    //this is default that runs for any scenario
+    @Before(order = 0) //it will get the name of Scenario that we run
+    public void setUp(Scenario scenario){
+        System.out.println("scenario = " + scenario.getSourceTagNames());
 
-    @Before
-    public void setUp(Scenario scenario){ // Scenario prints out the name of scenario from feature
-        System.out.println("Scenario name: " + scenario.getName());
-        System.out.println("Empty hook before");
+        System.out.println("scenario = " + scenario.getName());
+        System.out.println("I am setting up the test the Hooks class!! Before method running!!");
+
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //we can also add maximize screen here, but I already added at Driver class
+        Driver.getDriver().get(ConfigurationReader.getProperty("url" + ConfigurationReader.getProperty("environment")));
     }
 
-    @Before("@Scenario_1")
-    public void SpecialSetUp(){
-        System.out.println("Special hook before\n");
+
+    @After()
+    public void tearDown(Scenario scenario) {
+        System.out.println("Reporting with @After");
+        //I want to take screenshot when current scenario fails.
+        //scenario.isFailed() --> tels if the scenario failed or not.
+        if(scenario.isFailed()){
+
+            //Easy thee steps
+//            TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+//            byte[] image = takesScreenshot.getScreenshotAs(OutputType.BYTES);
+//            scenario.embed(image, "image/png");
+
+
+            //this is for taking screenshot
+            final byte[] screenshot =((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+         //   scenario.embed(screenshot,"image/png");
+
+
+        }
+        System.out.println("Closing driver with After method");
+        Driver.closeDriver();
     }
 
-    @After("Scenario_1")
-    public void SpecialTearDown(){
-        System.out.println("Special Empty hook after\n");
-    }
 
-    @After
-    public void tearDown(){
 
-        System.out.println("Empty hook after\n");
-    }
+
+
+    //this Hook will work only for scenarios with tag @negative
+    //also it will run before default hook
+    //because of priority (order=1)
+//    @Before(value = "@negative", order = 0)
+//    public void setUpDriver() {
+//        System.out.println("Before for negative");
+//    }
+
+
+
 }
