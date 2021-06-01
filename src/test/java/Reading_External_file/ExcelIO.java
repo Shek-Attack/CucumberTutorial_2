a0e639cfc9d44b9f4f7673aec9a286fb39f79773
Noorl <ab427902@dal.ca>
2021-06-01 19:45:50 -0400

    getting values from all rows

--- src/test/java/Reading_External_file/ExcelIO.java
+++ src/test/java/Reading_External_file/ExcelIO.java
@@ -9,26 +9,40 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ExcelIO {
+
+    //flow: workbook from workbookFactory
+    // workbook > sheet (getSheet) > row (getRow) > cell (getCell)
+    //
     @Test
     public void readDataFromExcel() throws IOException {
         //1 . import excel data from computer
-        String path ="C://Users/HomeExpensesMay23.xlsx";
+        String path = "S:/Cybertek Classessssssssssssss/HomeExpensesMay23.xlsx";
         Workbook workbook = WorkbookFactory.create(new File(path));
+
         //2. read data from excel
         Sheet sheet = workbook.getSheet("expense"); // select file from Excel
+
+        //3. get the row from sheet
         Row row = sheet.getRow(0);  // sheet>row>cell ; get the first row
+
+        //4. get the cell from row
         Cell cell = row.getCell(0); // sheet>row>cell ; get the first cell
 
-        System.out.println(cell.getStringCellValue()); //read value from the cell as string
-        System.out.println(sheet.getLastRowNum()); // get the last row number
-        int lastRow = sheet.getLastRowNum(); // get the last row number
+        //5. Read the cell value
+        System.out.println("The first cell from first row: " + cell.getStringCellValue()); //read value from the cell as string
+        //int firstRow = sheet.getFirstRowNum();
+
+        int lastRow = sheet.getLastRowNum();//get the last row column number
+        System.out.println("Last row value: " + sheet.getRow(lastRow).getCell(0));
+        System.out.println();
 
        // System.out.println(sheet.getRow(lastRow).getCell(0));
-        for(int i=0; i<lastRow;i++){
-            int lastColumn = sheet.getRow(i).getLastCellNum()-1; //get the column, then read the calue
+        for(int i=0; i<lastRow; i++){
+            int lastColumn = sheet.getRow(i).getLastCellNum() -1;// gets all the cells
 
-            System.out.println(sheet.getRow(i).getCell(0)); //get the all row values from first cell
-            System.out.print("  " + sheet.getRow(i).getCell(lastColumn));// get the all rows and columns
+            System.out.print(sheet.getRow(i).getCell(0));// get the value of first column
+            System.out.println(":    " + sheet.getRow(i).getCell(lastColumn));
+
             System.out.println();
         }
 
